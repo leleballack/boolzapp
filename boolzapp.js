af6c03a26d83eab50297065e35606ae006bc1486
leleballack <emanuele.ballacci@gmail.com>
2019-05-16 00:28:24 +0200

    Milestone 1

--- boolzapp.js
+++ boolzapp.js
@@ -1,1 +1,4 @@
-alert("ciao")
+// Replica della grafica con la possibilità di avere messaggi scritti
+// dall’utente (verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse
+// Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e
+// cliccando invia il testo viene aggiunto al thread sopra, come messaggio verde
