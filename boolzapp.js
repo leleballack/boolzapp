cd72eeda2c6a46b1ba7ca58f059a24e2b3aaf6d6
leleballack <emanuele.ballacci@gmail.com>
2019-05-16 20:47:39 +0200

    Milestone 2

--- boolzapp.js
+++ boolzapp.js
@@ -1,4 +1,41 @@
-// Replica della grafica con la possibilità di avere messaggi scritti
-// dall’utente (verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse
-// Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e
-// cliccando invia il testo viene aggiunto al thread sopra, come messaggio verde
+
+$(function (){
+  $(".fa-paper-plane").click(function(){
+    var text = $(".type_here").val();
+    if (text) {
+      var new_text = $(".template .message").clone();
+      new_text.children(".content").text(text);
+      var current_time = new Date();
+      new_text.children(".message_time").text(current_time.getHours() + ":" + current_time.getMinutes());
+      $(".message_box").append(new_text);
+    };
+    $(".type_here").val("");
+
+    setTimeout (function (){
+      var new_text = $(".template .message").clone();
+      new_text.toggleClass("green white");
+      new_text.children(".content").text("You're right!");
+      new_text.children(".message_time").text(current_time.getHours() + ":" + current_time.getMinutes());
+      $(".message_box").append(new_text);
+    }, 1500);
+
+  });
+    $(".type_here").keypress(function(e) {
+      var key = e.which;
+      if (key == 13) {
+        $(".fa-paper-plane").click();
+        return false;
+      }
+    });
+
+    $(".searchme").keyup(function() {
+      var lookup = $(this).val().toLowerCase();
+      $(".contact").each(function() {
+        if ($(this).find(".firstname").text().toLowerCase().includes(lookup)) {
+          $(this).show();
+        } else {
+          $(this).hide();
+        }
+      });
+    });
+});
